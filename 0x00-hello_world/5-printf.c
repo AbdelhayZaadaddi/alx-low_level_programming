3834b835e4429391340beb0c973f9643986681ac
abdelhay <abdelhay@gmail.com>
2023-06-15 02:33:30 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
 */
 int main()
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
