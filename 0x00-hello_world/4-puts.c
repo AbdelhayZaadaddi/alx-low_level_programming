f04995b1a7b94c1d60a91a63247942bbafa44008
abdelhay <abdelhay@gmail.com>
2023-06-15 02:17:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
 * Return: Always 0 (Success)
 */
 int main(void){
-puts("Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }
