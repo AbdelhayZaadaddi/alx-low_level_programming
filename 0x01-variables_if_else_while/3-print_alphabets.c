d1f7b12cd30fe971c32c811e7e8dedd6c1f248e8
abdelhay <abdelhay@gmail.com>
2023-06-16 06:40:09 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+* main - Prints the alphabet
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char low_letter = 'a';
+	char upp_letter = 'A';
+
+	while (low_letter <= 'z')
+	{
+		putchar(low_letter);
+		low_letter++;
+	}
+	while (upp_letter <= 'Z')
+	{
+		putchar(upp_letter);
+		upp_letter++;
+	}
+}
