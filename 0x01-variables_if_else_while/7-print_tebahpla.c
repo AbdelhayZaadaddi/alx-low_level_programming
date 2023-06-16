e372cddcbd95e3a9dfeca3b8349678929ddd13ef
abdelhay <abdelhay@gmail.com>
2023-06-16 07:49:52 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+
+	return (0);
+}
