48da8b5c83db69371a8ec941792c3fda06c2f6c2
abdelhay <abdelhay@gmail.com>
2023-06-16 07:08:27 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+* main - Prints the alphabet
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (!(letter == 'q' || letter == 'e'))
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	return (0);
+}
