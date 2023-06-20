1abae8d3738a7b84916a279efac196ef8a96fa1f
abdelhay <abdelhay@gmail.com>
2023-06-20 05:39:44 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ ** print_alphabetx10  - prints
+ *
+ * Description: prints the alphabet, in lowercase
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		int ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
