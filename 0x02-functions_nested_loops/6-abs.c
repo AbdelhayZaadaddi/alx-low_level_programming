b06cbb78f1bd3db8d71db2d1d884f55aac12a58c
abdelhay <abdelhay@gmail.com>
2023-06-20 07:46:59 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ ** _abs - function to check
+ *
+ * @n: cheks number
+ *
+ * Return: returns 1 if 'c' is lowercase, 0 (Success)
+ *
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n > 0)
+		n = n * (-1);
+	return (n);
+}
