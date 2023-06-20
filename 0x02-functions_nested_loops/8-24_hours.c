d907e4d29e2e51dbef49995bffa4689864f9fdd1
abdelhay <abdelhay@gmail.com>
2023-06-20 08:47:33 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,7 +18,7 @@ void jack_bauer(void)
 		{
 			_putchar((hr / 10) + 48);
 			_putchar((hr % 10) + 48);
-			_putchar(';');
+			_putchar(':');
 			_putchar((min / 10) + 48);
 			_putchar((min % 10) + 48);
 			_putchar('\n');
