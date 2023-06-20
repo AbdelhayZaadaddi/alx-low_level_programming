60b08bf9f21b469a21ddc5dd11381bc2ea4df0fd
abdelhay <abdelhay@gmail.com>
2023-06-20 07:02:12 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- ** _islower - function to check
+ ** _isalpha - function to check
  *
  * @c: cheks input of function
  *
@@ -11,7 +11,9 @@
 
 int _isalpha(int c)
 {
-	if (c >= 65 && <= 90 || c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
 		return (1);
 
 	return (0);
