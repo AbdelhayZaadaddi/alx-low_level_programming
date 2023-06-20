85d04fa7a1851ad462871e87d4f68bfb98622f7f
abdelhay <abdelhay@gmail.com>
2023-06-20 10:02:53 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ ** times_table - function to check
+ *
+ * Description: cheks input of function
+ *
+ */
+
+void times_table(void)
+{
+	int num, num2, mul;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0');
+		for (num2 = 1; num2 <= 9; num2++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			mul = num * num2;
+
+			if (mul <= 9)
+				_putchar(' ');
+			else
+				_putchar((mul / 10) + 48);
+			_putchar((mul % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
