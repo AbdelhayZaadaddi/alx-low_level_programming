1637b1da73625f776da180e8517d05d4e947d577
abdelhay <abdelhay@gmail.com>
2023-06-20 10:33:45 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ ** add - function to check
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return sum 0 (Success)
+ *
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
