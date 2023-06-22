de568cbb7654b95fd2de8b83eacdaa3dc965c571
abdelhay <abdelhay@gmail.com>
2023-06-22 04:08:37 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ ** print_most_numbers  - function that prints most numbers
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+	int m;
+
+	for (n = 1; n <= 10; n++)
+	{
+		for (m = 0; m <= 14; m++)
+		{
+			if (m >= 10)
+			{
+				_putchar('1');
+			}
+			_putchar(m % 10 + 48);
+		}
+		_putchar('\n');
+	}
+}
