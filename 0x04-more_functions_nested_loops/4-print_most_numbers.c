ccfccbec0d3d297a36ad5da57d8af061fd61cc28
abdelhay <abdelhay@gmail.com>
2023-06-22 03:19:45 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,13 +9,12 @@
 
 void print_most_numbers(void)
 {
-	int num;
+	char num;
 
-	for (num == 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		if (num == 2 || num == 4)
-			continue;
-		_putchar(num + 48);
+		if (!(num == '2' || num == '4'))
+			_putchar(num);
 	}
 	_putchar('\n');
 }
