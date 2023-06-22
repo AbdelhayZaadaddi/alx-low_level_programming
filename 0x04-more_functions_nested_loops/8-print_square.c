47060dee69ee60a662277f0541634621e382665f
abdelhay <abdelhay@gmail.com>
2023-06-22 06:27:50 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ ** print_line  - function that draws a straight line
+ *
+ * @n: number of times the character
+ *
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (i = 0; i < size; i++)
+	{
+		_putchar ( j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
