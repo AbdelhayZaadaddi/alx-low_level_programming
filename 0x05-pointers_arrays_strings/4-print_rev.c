0229a2bc89c0606110e3edf425eae65d81f389d6
abdelhay <abdelhay@gmail.com>
2023-06-26 06:57:11 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+		l++;
+	for (int i = l - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
