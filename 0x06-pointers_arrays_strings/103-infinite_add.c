88b0a61c22f373486f3921bd7d92a5abdd76bf8d
abdelhay <abdelhay@gmail.com>
2023-06-29 14:45:33 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * infinite_add - add 2 strings.
- * @n1: string1
- * @n2: string2
- * @r: buffer
- * @size_r: buffer size
+ * infinite_add - Add two strings representing large numbers.
+ * @n1: The first number as a string.
+ * @n2: The second number as a string.
+ * @r: The buffer to store the result.
+ * @size_r: The size of the buffer.
+ *
  * Return: Pointer to the result string on success, NULL if the result does not fit in the buffer.
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a_len = 0, b_len = 0;
-	int null = 0, a, b, sum, bg;
+	int a_len = 0, b_len = 0, null = 0, a, b, sum, bg;
 
 	while (n1[a_len] != '\0')
 		a_len++;
@@ -23,25 +23,24 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	else
 		bg = b_len;
 	if ((bg + 1) >= size_r)
-		return (0);
+		return (NULL);
 	r[bg + 1] = '\0';
 
 	while (bg >= 0)
 	{
-		a = (n1[a_len - 1] - '0');
-		b = (n1[b_len - 1] - '0');
+		a = (a_len > 0) ? (n1[a_len - 1] - '0') : 0;
+		b = (b_len > 0) ? (n2[b_len - 1] - '0') : 0;
 		if (a_len > 0 && b_len > 0)
 			sum = a + b;
-		else if (a_len < 0 && b_len > 0)
-			sum = b;
 		else if (a_len > 0 && b_len < 0)
 			sum = a;
+		else if (a_len < 0 && b_len > 0)
+			sum = b;
 		else
 			sum = null;
 
 		if (sum > 9)
-		{
-			null = sum / 10;
+		{null = sum / 10;
 			sum = (sum % 10) + '0';
 		}
 		else
@@ -54,7 +53,8 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		b_len--;
 		bg--;
 	}
-	if (*(r) != 0)
+
+	if (*r != '\0')
 		return (r);
 	else
 		return (r + 1);
