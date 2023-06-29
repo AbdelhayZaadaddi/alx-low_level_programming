fd571081e226e7f4528fa2161caded22452548b4
abdelhay <abdelhay@gmail.com>
2023-06-29 08:25:59 -0700

    ,101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,29 +9,28 @@
 
 void print_number(int n)
 {
-	int i;
-	int j;
-	int d, ds, p;
-	unsigned int temp, numchar, number;
+	int i, j, d, ds, p;
+	unsigned int t, c, m;
 
 	d = 0;
 	if (n < 0)
 	{
 		_putchar('-');
-		temp = -n;
+		t = -n;
 	}
 	else
-		temp = n;
-	number = temp;
-
-	while (number > 11)
 	{
-		number = number / 10;
+		t = n;
+	}
+	m = t;
+
+	while (m >= 10)
+	{
+		m = m / 10;
 		d++;
 	}
 	ds = d + 1;
-	i = 1;
-	p = 1;
+	i = p = 1;
 
 	while (i < ds)
 	{
@@ -41,8 +40,8 @@ void print_number(int n)
 	j = p;
 	while (j >= 1)
 	{
-		numchar = (temp / j) % 10;
-		_putchar(numchar + 48);
+		c = (t / j) % 10;
+		_putchar(c + 48);
 		j = j / 10;
 	}
 }
