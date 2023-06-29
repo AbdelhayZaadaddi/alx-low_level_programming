5d0d0b6ec2a7547c6fb4df798fa7358720f41b91
abdelhay <abdelhay@gmail.com>
2023-06-29 06:00:21 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - change letters to ROT13.
+ * @str: analized string.
+ * Return: String with all letters in ROT13 base.
+ */
+
+char *rot13(char *str)
+{
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j <= 51; j++)
+		{
+			if (s[1] == a[j])
+			{
+				str[i] = rot[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
