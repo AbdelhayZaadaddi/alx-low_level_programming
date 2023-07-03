79d13251d01363a34978cd91b467cf4b50f83c5c
abdelhay <abdelhay@gmail.com>
2023-07-03 04:59:57 -0700

    function that prints the chessboard.

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that locates a substring.
+ * @a: first value char
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 8; n++)
+	{
+		for (m = 0; m < 8; m++)
+		{
+			putchar(a[n][m]);
+		}
+		putchar('\n')
+	}
+}
