2d534cff11416ca3d05c3d70693c56423ff0ad3b
abdelhay <abdelhay@gmail.com>
2023-07-07 04:43:26 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The argument count
+ * @argv: The argument vector
+ *
+ * Description: This function prints the program name
+ *
+ * Return: 0 (indicating successful execution)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
