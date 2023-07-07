1b12c80f5608a68b09dac79873ffe50ea2c2917d
abdelhay <abdelhay@gmail.com>
2023-07-07 04:45:21 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,7 @@
  *
  * Return: 0 (indicating successful execution)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
