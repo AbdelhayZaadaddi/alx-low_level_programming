aa3d72d163b2a3d3f036d1f1968c7f8b141fcf49
abdelhay <abdelhay@gmail.com>
2023-07-07 06:33:37 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The argument count
+ * @argv: The argument vector
+ *
+ * Description: This function calculates the sum of all command-line arguments
+ *
+ * Return: 0 (indicating successful execution) if all arguments are valid integers
+ */
+
+int main(int argc, char *argv[])
+{
+	int n = 0, i, j;
+
+	for(i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		n = n + atoi(argv[i]);
+	}
+	printf("%d\n", n);
+	return (0);
+}
