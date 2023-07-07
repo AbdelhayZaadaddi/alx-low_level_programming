6c6bf2b673718c6d74b250346d721e492c8384ba
abdelhay <abdelhay@gmail.com>
2023-07-07 05:10:30 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point of the program
@@ -14,7 +15,7 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for(i = 1; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
