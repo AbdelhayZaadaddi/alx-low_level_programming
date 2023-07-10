4dc219e8af1e862164c4580bf5f656b58997ec18
abdelhay <abdelhay@gmail.com>
2023-07-10 08:00:15 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -11,27 +11,27 @@
 
 char *argstostr(int ac, char **av)
 {
-	if (ac == 0 || av == NULL)
-		return NULL;
+    if (ac == 0 || av == NULL)
+        return NULL;
 
-	int total_length = 0;
-	int position = 0;
-	int i;
+    int total_length = 0;
+    int i;
 
-	for (i = 0; i < ac; i++)
-		total_length += strlen(av[i]) + 1;
+    for (i = 0; i < ac; i++)
+        total_length += strlen(av[i]) + 1;
 
-	char *result = (char *)malloc(total_length + 1);
-	if (result == NULL)
-		return NULL;
+    char *result = (char *)malloc((total_length + 1) * sizeof(char));
+    if (result == NULL)
+        return NULL;
 
-	for (i = 0; i < ac; i++)
-	{
-		strcpy(result + position, av[i]);
-		position += strlen(av[i]);
-		result[position++] = '\n';
-	}
+    int position = 0;
+    for (i = 0; i < ac; i++)
+    {
+        strcpy(result + position, av[i]);
+        position += strlen(av[i]);
+        result[position++] = '\n';
+    }
 
-	result[total_length] = '\0';
-	return result;
+    result[total_length] = '\0';
+    return result;
 }
