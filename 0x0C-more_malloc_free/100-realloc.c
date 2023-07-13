8cf38e4a332b58b509bdb6832834b0b04b8cf0d9
abdelhay <abdelhay@gmail.com>
2023-07-13 05:30:59 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,30 +13,24 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
-
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	if (ptr == NULL)
+	void *new_ptr = malloc(new_size);
+
+	if (new_ptr == NULL)
 	{
-		new_ptr = malloc(new_size);
+		return (NULL);
 	}
-	else if (new_size <= old_size)
+
+	if (ptr != NULL)
 	{
-		new_ptr = ptr;
+		memcpy(new_ptr, ptr, (old_size < new_size) ? old_size : new_size);
+		free(ptr);
 	}
-	else
-	{
-		new_ptr = malloc(new_size);
-		if (new_ptr != NULL)
-		{
-			memcpy(new_ptr, ptr, old_size);
-			free(ptr);
-		}
-	}
+
 	return (new_ptr);
 }
