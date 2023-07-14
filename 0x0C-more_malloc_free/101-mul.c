8aca369652479a9a94baaf962b473409a47cc34d
abdelhay <abdelhay@gmail.com>
2023-07-13 17:42:17 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,60 +1,114 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <string.h>
 
 /**
- * is_digits - a function that checks if a buffer has only digits.
- * @buffer: buffer that will be tested.
- * Return: 0 if a non-digit was found, 1 otherwise
+ * _isdigit - checks if character is digit
+ * @c: the character to check
+ *
+ * Return: 1 if digit, 0 otherwise
  */
-int is_digits(char *buffer)
+int _isdigit(int c)
 {
-	int it = 0;
-
-	while (buffer[it] != '\0')
-	{
-		if (!(buffer[it] >= '0' && buffer[it] <= '9'))
-			return (0);
-
-		it++;
-	}
-
-	return (it);
+	return (c >= '0' && c <= '9');
 }
 
 /**
- * main - a program that multiplies two numbers.
- * @argc: count of arguments given to the programs
- * @argv: arguments given to the program.
- * Return: Always 0.
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: integer length of string
  */
-int main(int argc, char *argv[])
+int _strlen(char *s)
 {
+	int i = 0;
 
-	char *buff1;
-	char *buff2;
-	int count_1, count_2;
+	while (*s++)
+		i++;
+	return (i);
+}
 
-	if (argc < 3)
+/**
+ * big_multiply - multiply two big number strings
+ * @s1: the first big number string
+ * @s2: the second big number string
+ *
+ * Return: the product big number string
+ */
+char *big_multiply(char *s1, char *s2)
+{
+	char *r;
+	int l1, l2, a, b, c, x;
+
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
+	r = malloc(a = x = l1 + l2);
+	if (!r)
+		printf("Error\n"), exit(98);
+	while (a--)
+		r[a] = 0;
+
+	for (l1--; l1 >= 0; l1--)
 	{
-		printf("Error\n");
-		return (98);
+		if (!_isdigit(s1[l1]))
+		{
+			free(r);
+			printf("Error\n"), exit(98);
+		}
+		a = s1[l1] - '0';
+		c = 0;
+
+		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
+		{
+			if (!_isdigit(s2[l2]))
+			{
+				free(r);
+				printf("Error\n"), exit(98);
+			}
+			b = s2[l2] - '0';
+
+			c += r[l1 + l2 + 1] + (a * b);
+			r[l1 + l2 + 1] = c % 10;
+
+			c /= 10;
+		}
+		if (c)
+			r[l1 + l2 + 1] += c;
 	}
+	return (r);
+}
 
-	buff1 = argv[1];
-	buff2 = argv[2];
-	count_1 = is_digits(buff1);
-	count_2 = is_digits(buff2);
 
-	if ((count_1 == 0) || (count_2 == 0))
+/**
+ * main - multiply two big number strings
+ * @argc: the number of arguments
+ * @argv: the argument vector
+ *
+ * Return: Always 0 on success.
+ */
+int main(int argc, char **argv)
+{
+	char *r;
+	int a, c, x;
+
+	if (argc != 3)
+		printf("Error\n"), exit(98);
+
+	x = _strlen(argv[1]) + _strlen(argv[2]);
+	r = big_multiply(argv[1], argv[2]);
+	c = 0;
+	a = 0;
+	while (c < x)
 	{
-		printf("Error\n");
-		return (98);
+		if (r[c])
+			a = 1;
+		if (a)
+			_putchar(r[c] + '0');
+		c++;
 	}
-
-	if (count_1 > 19 || count_2 > 19)
-		return (0);
-
-	printf("%i\n", atoi(buff1) * atoi(buff2));
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(r);
 	return (0);
 }
