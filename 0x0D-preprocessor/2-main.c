8a645d29432b40d75fdf4903f26189c6c6e7e21b
abdelhay <abdelhay@gmail.com>
2023-07-17 04:35:55 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function is the entry point of the program.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
