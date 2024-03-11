deb4cfd2a0d3ac932a802fe87e8f668be2077768
Abdelhay Zaadaddi <abdezaadaddi06@.com>
2024-03-11 14:51:24 -0700

    oool

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,10 +14,11 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	int l = 0;
-	int h = size -1;
-	int mid, j;
 	size_t i;
+	int l = 0;
+	int h = size - 1;
+	int mid;
+	int j;
 
 	if (!array)
 		return (-1);
@@ -25,27 +26,23 @@ int binary_search(int *array, size_t size, int value)
 	for (i = 0; l <= h; i++)
 	{
 		mid = (l + h) / 2;
-		printf("Searcing in array: ");
+
+
+		printf("Searching in array: ");
 		for (j = l; j <= h; j++)
 		{
 			printf("%d", array[j]);
 			if (j != h)
-			{
 				printf(", ");
-			}
 		}
 		printf("\n");
 
 		if (array[mid] == value)
 			return (array[mid]);
-		else if (array[mid] < value)
-		{
-			l = mid + 1;
-		}
-		else
-		{
+		else if (array[mid] > value)
 			h = mid - 1;
-		}
+		else
+			l = mid + 1;
 	}
 
 	return (-1);
